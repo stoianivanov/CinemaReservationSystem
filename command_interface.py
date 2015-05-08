c04616658895b2ac5547ea70421645f2449b9941
Stoian <stoian_ivanov_ivanov@abv.bg>
2015-05-08 14:51:46 +0300

    dsa

--- command_interface.py
+++ command_interface.py
@@ -2,24 +2,25 @@ import sqlite3
 
 
 db = sqlite3.connect("cinema_data.db")
+db.row_factory = sqlite3.Row
 cursor = db.cursor()
 
 
 def take_available_seats():
     seats = [
-        ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.']
-        ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.']
-        ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.']
-        ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.']
-        ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.']
-        ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.']
-        ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.']
-        ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.']
-        ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.']
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.']
             ]
     take_row_col = """
-    SELECT row, col FROM Projections
+    SELECT row, col FROM Reservations
     """
     info = cursor.execute(take_row_col)
 
@@ -28,12 +29,63 @@ def take_available_seats():
         seats_col = row['col']
         seats[seats_row][seats_col] = 'X'
 
-    print("  1 2 3 4 5 6 7 8 9")
-    for i in range(0, 11):
-        print (i + " ")
-        for j in range(0, 11):
-            print (j + ' ')
-            print(seats[i][j] + ' ')
+    print("  1    2    3    4    5    6    7    8    9    10")
+    for i in range(0, len(seats)):
+        print(seats[i])
+
+
+def take_seat(username, projection_id, row, col):
+    take_row_col = """
+    SELECT row, col FROM Reservations
+    """
+    info = cursor.execute(take_row_col)
+
+    for line in info:
+        seats_row = line['row']
+        seats_col = line['col']
+        if row == seats_row and col == seats_col:
+            print ('Lol...NO!')
+            return False
+
+    cursor.execute("""
+    INSERT INTO Reservations(username, projection_id, row, col)
+    VALUES(?, ?, ?, ?)
+    """, (username, str(projection_id), str(row), str(col)))
+    db.commit()
+    return True
+
+
+def choose_seat(username, projection_id, number):
+    counter = 1
+    seats = []
+    while number > 0:
+        print ('Step 4 (Seats): Choose seat {}>'. format(counter))
+        row = int(input()) - 1
+        col = int(input()) - 1
+        if take_seat(username, projection_id, row, col):
+            number -= 1
+            counter += 1
+            seats.append((row, col))
+    else:
+        info = cursor.execute("""
+        SELECT projection_type, projection_date, projection_time,
+        movie_name, movie_rating
+        FROM Projections
+        JOIN Movies
+        ON Projections.movie_id = Movies.id
+        WHERE Projections.id = ?
+        """, str(projection_id))
+        db.commit()
+        for row in info:
+            print ('This is your reservation:')
+            print ('Movie: {} ({})'.format(row['movie_name'],row['movie_rating']))
+            print ('Date and Time: {} ({})'.format(row['projection_date'], row['projection_type']))
+            print ('Seats: {}'. format(seats))
+
+
+take_available_seats()
+choose_seat("pesho", 2, 4)
+take_available_seats()
 
 
 def make_reservation():
@@ -44,6 +96,9 @@ def make_reservation():
     show_movies()
 
     movie_id = input("Step 2 (Movie): Choose a movie>")
-    #show_movies_projections
+    show_movies_projections(movie_id)
 
-    jection_id = input("Step 3 (Projection): Choose a projection>")
+    projection_id = input("Step 3 (Projection): Choose a projection>")
+    take_available_seats()
+
+    choose_seat(username, projection_id, number_of_tickets)
